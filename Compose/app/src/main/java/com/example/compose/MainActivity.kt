b3fd7ce831cb836b11826c0837ca95c369ce63ec
jisang <jisangman89@gmail.com>
2021-10-29 00:47:08 +0900

    # update: [compose] 기본 개념 및 ui 학습 (Column, Row, Image etc)

--- Compose/app/src/main/java/com/example/compose/MainActivity.kt
+++ Compose/app/src/main/java/com/example/compose/MainActivity.kt
@@ -3,11 +3,18 @@ package com.example.compose
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.compose.ui.theme.ComposeTheme
 
 class MainActivity : ComponentActivity() {
@@ -17,7 +24,9 @@ class MainActivity : ComponentActivity() {
             ComposeTheme {
                 // A surface container using the 'background' color from the theme
                 Surface(color = MaterialTheme.colors.background) {
-                    Greeting("Android")
+//                    Greeting("Android")
+//                    SetImage()
+                    SetDetailImage()
                 }
             }
         }
@@ -26,13 +35,56 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun Greeting(name: String) {
-    Text(text = "Hello $name!")
+    Column {
+        Text(text = "Hello $name!")
+        Text(text = "Hello!!! $name!")
+    }
+}
+
+@Composable
+fun SetImage() {
+    Row {
+        Image(
+            painter = painterResource(
+                id = R.drawable.ic_launcher_foreground
+            ),
+            contentDescription = "default image"
+        )
+    }
+}
+
+@Composable
+fun SetDetailImage() {
+    // Add padding around our message
+    Row(modifier = Modifier.padding(all = 8.dp)) {
+        Image(
+            painter = painterResource(R.drawable.ic_launcher_background),
+            contentDescription = "Contact profile picture",
+            modifier = Modifier
+                // Set image size to 40 dp
+                .size(40.dp)
+                // Clip image to be shaped as a circle
+                .clip(CircleShape)
+        )
+
+        // Add a horizontal space between the image and the column
+        Spacer(modifier = Modifier.width(8.dp))
+
+        Column {
+            Text(text = "jay")
+            // Add a vertical space between the author and message texts
+            Spacer(modifier = Modifier.height(4.dp))
+            Text(text = "you")
+        }
+    }
 }
 
 @Preview(showBackground = true)
 @Composable
 fun DefaultPreview() {
     ComposeTheme {
-        Greeting("Android")
+//        Greeting("Android")
+//        SetImage()
+        SetDetailImage()
     }
 }
