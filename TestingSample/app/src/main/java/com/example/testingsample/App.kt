7c4b4b4a0b23e7fde8e4f284fb9595bd9f38834e
jisang <jisangman89@gmail.com>
2024-01-05 14:31:29 +0900

    delete : TestSample, Test_Basic

--- TestingSample/app/src/main/java/com/example/testingsample/App.kt
+++
@@ -1,8 +0,0 @@
-package com.example.testingsample
-
-import android.app.Application
-import dagger.hilt.android.HiltAndroidApp
-
-@HiltAndroidApp
-class App : Application() {
-}
--- TestingSample/app/src/main/java/com/example/testingsample/Calculator.kt
+++
@@ -1,6 +0,0 @@
-package com.example.testingsample
-
-class Calculator {
-
-    fun sum(a: Int, b: Int) = a + b
-}
