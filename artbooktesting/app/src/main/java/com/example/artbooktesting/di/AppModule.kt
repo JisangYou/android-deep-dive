c1b6a8482618722bb2ede55436c982f0f6226b66
jisang <jisangman89@gmail.com>
2024-01-25 23:56:04 +0900

    # update : adapter
    udemy - Android Testing

--- artbooktesting/app/src/main/java/com/example/artbooktesting/di/AppModule.kt
+++ artbooktesting/app/src/main/java/com/example/artbooktesting/di/AppModule.kt
@@ -2,7 +2,13 @@ package com.example.artbooktesting.di
 
 import android.content.Context
 import androidx.room.Room
+import com.bumptech.glide.Glide
+import com.bumptech.glide.request.RequestOptions
+import com.example.artbooktesting.R
 import com.example.artbooktesting.api.RetrofitAPI
+import com.example.artbooktesting.repo.ArtRepository
+import com.example.artbooktesting.repo.ArtRepositoryInterface
+import com.example.artbooktesting.roomdb.ArtDao
 import com.example.artbooktesting.roomdb.ArtDatabase
 import com.example.artbooktesting.util.Util.BASE_URL
 import dagger.Module
@@ -39,4 +45,15 @@ object AppModule {
             .create(RetrofitAPI::class.java)
     }
 
+    @Singleton
+    @Provides
+    fun injectNormalRepo(dao : ArtDao, api: RetrofitAPI) = ArtRepository(dao,api) as ArtRepositoryInterface
+
+    @Singleton
+    @Provides
+    fun injectGlide(@ApplicationContext context: Context) = Glide
+        .with(context).setDefaultRequestOptions(
+            RequestOptions().placeholder(R.drawable.ic_launcher_foreground)
+                .error(R.drawable.ic_launcher_foreground)
+        )
 }
